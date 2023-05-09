a1863caa758994a841c936a36d4acf5c00ea8f19
Your Name <you@example.com>
2023-05-09 16:27:16 -0500

    agregando consulta de inicio de sesion

--- src/app.js
+++ src/app.js
@@ -47,7 +47,6 @@ app.get('/api/producto', (req, res)=>{
 })
 
 //Ver tabla factura
- 
 app.get('/api/factura', (req, res)=>{
     conexion.query('SELECT  CONCAT(c.nombre," ",c.apellido) as "NombreCliente", p.nombre as "producto", m.metodo, cantidad from factura f INNER JOIN cliente c ON f.id_cliente = c.id_cliente INNER JOIN producto p ON f.id_producto = p.id_producto INNER JOIN medio_pago m ON f.id_pago = m.id_pago;', (error, filas)=>{
         if(error){
@@ -57,6 +56,57 @@ app.get('/api/factura', (req, res)=>{
         }
     })
 })
+
+/*Inicio de sesion
+app.post('/api/login', (req, res) => {
+    // Obtener el nombre de usuario y contraseña del cuerpo de la solicitud
+    const { usuario, passwor } = req.body;
+  
+    // Realizar la consulta para validar el inicio de sesión
+    conexion.query('SELECT * FROM empleado WHERE usuario = ? AND passwor = ?', [usuario, passwor], (error, filas) => {
+      if (error) {
+        throw error;
+      } else {
+        // Verificar si se encontró un empleado con las credenciales proporcionadas
+        if (filas.length > 0) {
+          // Credenciales válidas, inicio de sesión exitoso
+          res.json({ message: 'Inicio de sesión exitoso' });
+        } else {
+          // Credenciales inválidas, inicio de sesión fallido
+          res.status(401).json({ message: 'Credenciales inválidas' });
+        }
+      }
+    });
+  });
+*/  
+
+//Inicio de sesion
+app.post('/api/login', (req, res) => {
+    const { usuario, passwor } = req.body;
+    // Realizar la consulta para validar el inicio de sesión
+    conexion.query(
+      'SELECT usuario, passwor, id_rol FROM empleado WHERE usuario = ? AND passwor = ?',
+      [usuario, passwor],
+      (error, filas) => {
+        if (error) {
+          throw error;
+        } else {
+          if (filas.length > 0) {
+            const empleado = filas[0];
+            res.json({
+              message: 'Inicio de sesión exitoso',
+              rol: empleado.id_rol // Pasar id del rol en la respuesta
+            });
+          } else {
+            res.status(401).json({ message: 'Credenciales inválidas' });
+          }
+        }
+      }
+    );
+});
+  
+  
+
 
 app.listen(PORT)
 console.log('server on port', PORT)
